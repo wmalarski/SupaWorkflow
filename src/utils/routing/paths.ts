353e6e320f3d9345de3f131412bfe1579bd7806c
Wojciech Malarski <w.malarski@gmail.com>
2021-08-22 19:46:53 +0200

    renaming organization route to dashboard

--- src/utils/routing/paths.ts
+++ src/utils/routing/paths.ts
@@ -1,25 +1,25 @@
 const paths = {
   template: (organizationId: number, templateId: number): string =>
-    `/organization/${organizationId}/template/${templateId}`,
+    `/dashboard/${organizationId}/template/${templateId}`,
   newWorkflow: (organizationId: number, templateId: number): string =>
-    `/organization/${organizationId}/template/${templateId}/new`,
+    `/dashboard/${organizationId}/template/${templateId}/new`,
   templates: (organizationId: number): string =>
-    `/organization/${organizationId}/template`,
+    `/dashboard/${organizationId}/template`,
   newTemplate: (organizationId: number): string =>
-    `/organization/${organizationId}/template/new`,
+    `/dashboard/${organizationId}/template/new`,
   workflow: (organizationId: number, workflowId: number): string =>
-    `/organization/${organizationId}/workflow/${workflowId}`,
+    `/dashboard/${organizationId}/workflow/${workflowId}`,
   workflows: (organizationId: number): string =>
-    `/organization/${organizationId}/workflow`,
+    `/dashboard/${organizationId}/workflow`,
   organization: (organizationId: number): string =>
-    `/organization/${organizationId}`,
+    `/dashboard/${organizationId}`,
   organizationMembers: (organizationId: number): string =>
-    `/organization/${organizationId}/members`,
+    `/dashboard/${organizationId}/members`,
   organizationSettings: (organizationId: number): string =>
-    `/organization/${organizationId}/settings`,
-  organizations: "/organization",
-  newOrganization: "/organization/new",
-  profile: "/organization/profile",
+    `/dashboard/${organizationId}/settings`,
+  organizations: "/dashboard",
+  newOrganization: "/dashboard/new",
+  profile: "/dashboard/profile",
   home: "/",
   signIn: "/signIn",
   signUp: "/signUp",
