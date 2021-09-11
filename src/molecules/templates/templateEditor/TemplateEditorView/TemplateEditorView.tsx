6beadab8f0a3f2c64705f648caaa4858f366822c
Wojciech Malarski <w.malarski@gmail.com>
2021-09-11 10:39:30 +0200

    react flow setup

--- src/molecules/templates/templateEditor/TemplateEditorView/TemplateEditorView.tsx
+++ src/molecules/templates/templateEditor/TemplateEditorView/TemplateEditorView.tsx
@@ -1,9 +1,13 @@
-import { StackDivider, VStack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import ReactFlow, {
+  addEdge,
+  Connection,
+  Edge,
+  Elements,
+  removeElements,
+} from "react-flow-renderer";
 import { Message } from "../../../../services";
 import { MutationArgs } from "../../../../utils/rep";
-import MessageForm from "../MessageForm/MessageForm";
-import MessageListItem from "../MessageListItem/MessageListItem";
 
 export type TemplateEditorViewProps = {
   templateId: number;
@@ -12,25 +16,70 @@ export type TemplateEditorViewProps = {
   onDeleteClick: (args: MutationArgs["delMessage"]) => void;
 };
 
+const initialElements: Elements = [
+  {
+    id: "1",
+    type: "input", // input node
+    data: { label: "Input Node" },
+    position: { x: 250, y: 25 },
+  },
+  // default node
+  {
+    id: "2",
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+    label: "Label456",
+  },
+  {
+    id: "3",
+    type: "output", // output node
+    data: { label: "Output Node" },
+    position: { x: 250, y: 250 },
+  },
+  // animated edge
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e2-3", source: "2", target: "3", label: "Label123" },
+];
+
 const TemplateEditorView = ({
   templateId,
   messages,
   onMessageChange,
   onDeleteClick,
-}: TemplateEditorViewProps): JSX.Element => (
-  <>
-    <MessageForm templateId={templateId} onCreateClick={onMessageChange} />
-    <VStack divider={<StackDivider borderColor="gray.200" />}>
-      {messages.map((message) => (
-        <MessageListItem
-          key={message.id}
-          message={message}
-          onMessageChange={onMessageChange}
-          onDeleteClick={onDeleteClick}
-        />
-      ))}
-    </VStack>
-  </>
-);
+}: TemplateEditorViewProps): JSX.Element => {
+  const [elements, setElements] = useState<Elements>(initialElements);
+
+  const onElementsRemove = (elementsToRemove: Elements) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+
+  const onConnect = (params: Edge | Connection) =>
+    setElements((els) => addEdge(params, els));
+
+  return (
+    <div style={{ height: 300 }}>
+      <ReactFlow
+        elements={elements}
+        onElementsRemove={onElementsRemove}
+        onConnect={onConnect}
+        snapToGrid
+        deleteKeyCode={46} /* 'delete'-key */
+      />
+    </div>
+    // <>
+    //   <MessageForm templateId={templateId} onCreateClick={onMessageChange} />
+    //   <VStack divider={<StackDivider borderColor="gray.200" />}>
+    //     {messages.map((message) => (
+    //       <MessageListItem
+    //         key={message.id}
+    //         message={message}
+    //         onMessageChange={onMessageChange}
+    //         onDeleteClick={onDeleteClick}
+    //       />
+    //     ))}
+    //   </VStack>
+    // </>
+  );
+};
 
 export default TemplateEditorView;
